7b8afef788aae5caaaa37f57c9c48a393b6d897a
chienpv <phamvanbk11@gmail.com>
2025-01-20 10:27:53 +0700

    demo using jpa

--- src/main/java/HelloServelet.java
+++ src/main/java/HelloServelet.java
@@ -1,18 +1,32 @@
 import java.io.IOException;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.User;
 
 public class HelloServelet extends HttpServlet {
 @Override
 public void doGet(HttpServletRequest req, HttpServletResponse res) {
+    EntityManagerFactory factory = Persistence.createEntityManagerFactory("demoPU");
+    EntityManager em = factory.createEntityManager();
+    em.getTransaction().begin();
+    try {
+            String jpaql = "SELECT o FROM User o";
+            TypedQuery<User> query = em.createQuery(jpaql, User.class);
+            List<User> list =  query.getResultList();
+            req.setAttribute("list", list);
+        } catch (Exception e) {
+        // TODO: handle exception
+    }
     RequestDispatcher view = req.getRequestDispatcher("/hello.jsp");      
     try {
        view.forward(req, res);
